7586b4d55ff669ba274747d55706aaa93a14b6de
Denise Pendleton <dpendl1@hotmail.com>
2018-05-29 16:58:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 
 class BoardMember{
   constructor(name, homeState, training){
-    this.name
-    this.homeState
-    this.training
+    this.name = name
+    this.homeState = homeState
+    this.training = training
   }
 
   veto(){
     return "No, I must disagree"
   }
 
-  
-}
 
+}
