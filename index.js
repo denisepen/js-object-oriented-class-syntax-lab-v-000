a67dd5a8484457f1f12a084041ddaf3911a84ff9
Denise Pendleton <dpendl1@hotmail.com>
2018-05-29 16:59:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,13 +9,17 @@ class BoardMember{
   veto(){
     return "No, I must disagree"
   }
-  
+
   approve(){
     return "You can do that!"
   }
 
   doCharity(){
     return "I like to help people."
+  }
+
+  releasePressStatement(){
+    return "You will see great things from Scuber."
   }
 
 
